6c63d323b7a0269588abb43fd7db2d565cf7353f
Alex Villarejo i Garcia <mail@villarejoigarcia.com>
2026-02-03 12:51:22 +0100

    archive fix

--- src/app/(main)/archive/archive.tsx
+++ src/app/(main)/archive/archive.tsx
@@ -19,6 +19,9 @@ export default function ArchiveList({ data }: ArchiveListProps) {
   const [visibleSet, setVisibleSet] = useState<Set<string>>(new Set());
   const [scales, setScales] = useState<Record<string, number>>({});
 
+  const [showText, setShowText] = useState<boolean>(false);
+  const [textValue, setTextValue] = useState<string | null>(null);
+
   const hasShuffled = useRef(false);
   const imgRefs = useRef<Map<string, HTMLDivElement>>(new Map());
 
@@ -75,6 +78,19 @@ export default function ArchiveList({ data }: ArchiveListProps) {
     };
   }, [zoomImg]);
 
+  useEffect(() => {
+    if (zoomImg) {
+      setTextValue(zoomImg);
+      setShowText(true);
+    } else {
+      const timeout = setTimeout(() => {
+        setShowText(false);
+        setTextValue(null);
+      }, 500);
+      return () => clearTimeout(timeout);
+    }
+  }, [zoomImg]);
+
   function handleImageLoad(e: React.SyntheticEvent<HTMLImageElement, Event>, url: string) {
     const img = e.currentTarget;
     const naturalWidth = img.naturalWidth;
@@ -85,7 +101,7 @@ export default function ArchiveList({ data }: ArchiveListProps) {
     let scale = 1;
 
     // Clasificar en tres rangos: Extremas, Horizontal, Vertical
-    const isExtreme = naturalHeight >= 2 * naturalWidth;
+    const isExtreme = naturalHeight >= 1.5 * naturalWidth;
     const isHorizontal = naturalWidth > naturalHeight;
     const isVertical = naturalHeight >= naturalWidth;
 
@@ -120,16 +136,16 @@ export default function ArchiveList({ data }: ArchiveListProps) {
           const isVisible = visibleSet.has(img.asset.url);
 
           let opacityClass = 'opacity-0';
-          let blurClass = 'blur-xs';
+          let blurClass = 'blur-none';
           let zIndex = 'z-0 delay-0'
 
           if (isZoomed) {
             opacityClass = 'opacity-100';
             blurClass = 'blur-none';
-            zIndex = 'z-[8]';
+            zIndex = 'z-[9]';
           } else if (zoomImg && isVisible) {
             opacityClass = 'opacity-10';
-            blurClass = 'blur-xs';
+            blurClass = 'blur-none';
           } else if (!zoomImg && isVisible) {
             opacityClass = 'opacity-100';
             blurClass = 'blur-none';
@@ -193,14 +209,18 @@ export default function ArchiveList({ data }: ArchiveListProps) {
                 className={`w-full h-auto transition-all duration-500 ease-in-out position-absolute ${opacityClass} ${blurClass} ${originClass}`}
                 style={{ transform: isZoomed ? `scale(${dynamicScale})` : 'scale(1)' }}
               />
-              <div className={`${isZoomed && fileName ? 'opacity-100' : 'opacity-0'} transition duration-500 pointer-events-none fixed left-[50vw] translate-x-[-50%] bottom-(--kv)`}>
+              {/* <p className={`${isZoomed && fileName ? 'opacity-100' : 'opacity-0'} z-100 transition duration-500 pointer-events-none fixed left-[50vw] translate-x-[-50%] bottom-(--kv)`}>
                 {fileName}
-              </div>
+              </p> */}
             </div>
 
           );
         })}
       </div>
+
+  <p className={`fixed left-[50vw] translate-x-[-50%] bottom-(--kv) z-100 pointer-events-none transition-opacity duration-500 ${zoomImg ? 'opacity-100' : 'opacity-0'}`}>
+    {showText && textValue ? textValue.split('/').pop() : ''}
+  </p>
     </>
   );
 }
