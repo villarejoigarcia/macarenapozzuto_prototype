48a88d381c9a6f36269a0ced3d40ca57e5aaa79d
Alex Villarejo i Garcia <mail@villarejoigarcia.com>
2026-02-27 18:29:07 +0100

    scroll test

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,94 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useRef, useState, useEffect, useLayoutEffect } from "react";
+import { motion, useScroll, useTransform, useSpring } from "framer-motion";
+import { div } from "framer-motion/client";
+
+function ScrollItem({ index }: { index: number }) {
+  const ref = useRef<HTMLDivElement>(null);
+  const textRef = useRef<HTMLDivElement>(null);
+  const [textHeight, setTextHeight] = useState(0);
+  const { scrollY } = useScroll();
+
+  const scaleRaw = useTransform(scrollY, () => {
+    if (!ref.current) return 0.333;
+
+    const rect = ref.current.getBoundingClientRect();
+    const viewportCenter = window.innerHeight / 2;
+    const elementCenter = rect.top + rect.height / 2;
+
+    const distance = Math.abs(viewportCenter - elementCenter);
+    const maxDistance = window.innerHeight / 2;
+    const normalized = Math.min(distance / maxDistance, 1);
+
+    return 1 - normalized * 0.667;
+  });
+
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = scrollY.on("change", () => {
+      if (!ref.current) return;
+
+      const rect = ref.current.getBoundingClientRect();
+      const viewportTrigger = window.innerHeight / 2;
+
+      const active =
+        rect.top <= viewportTrigger && rect.bottom >= viewportTrigger;
+
+      setIsActive(active);
+    });
+
+    return () => unsubscribe();
+  }, [scrollY]);
+
+  useLayoutEffect(() => {
+    if (textRef.current) {
+      setTextHeight(textRef.current.scrollHeight);
+    }
+  }, []);
+
+  const widthSpring = useSpring(scaleRaw, {
+    stiffness: 500,
+    damping: 200,
+    mass: 1,
+    bounce: 0,
+  });
+
+  const width = useTransform(widthSpring, [0.333, 1], ["33.333%", "100%"]);
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div className={`flex flex-col items-center lg:px-[33.333vw] relative`}>
+      <motion.div
+        ref={ref}
+        style={{ width }}
+        className="item w-full"
+      >
+        <img
+          src={`/images/image${index}.webp`}
+          style={{ width: "100%", height: "auto" }}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      </motion.div>
+
+      <div ref={textRef}
+        style={{ height: isActive ? textHeight : 0 }}
+        className={`overflow-hidden transition-all duration-500 flex w-full box-border px-[10px] ${isActive ? 'my-[10px_5px]' : 'p-0'} relative lg:absolute top-full left-0`}
+      >
+        <p className="flex-1 grow-2 lg:grow-3">Fotosprint</p>
+        <p className="flex-1">Brand identity</p>
+        <p className="flex-0 text-right">2025</p>
+      </div>
+
+    </div>
+  );
+}
+
+export default function Page() {
+  return (
+    <div className="wrapper flex flex-col gap-[5px] items-center py-[51dvh]">
+      {[1, 2, 3, 4, 5, 6, 7, 8].map((i) => (
+        <ScrollItem key={i} index={i} />
+      ))}
     </div>
   );
 }
